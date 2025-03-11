818c10a257dbbb06df2f2152dab66979f9f676dc
Doris Bilic <dorisbilic11@gmail.com>
2025-03-11 18:00:54 +0100

    Add RequestCategoryDto and ResponseCategoryDto classes

---
+++ src/main/java/com/dikolacici/DI_kolacici/controller/response/ResponseCategoryDto.java
@@ -0,0 +1,15 @@
+package com.dikolacici.DI_kolacici.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseCategoryDto {
+
+    private Long id;
+
+    private String categoryName;
+}
