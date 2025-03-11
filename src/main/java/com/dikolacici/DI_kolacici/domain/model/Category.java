48e609cbf59b468fbd2941b11222d9c0b3ff10a7
Doris Bilic <dorisbilic11@gmail.com>
2025-03-11 17:59:43 +0100

    Add Category entity

---
+++ src/main/java/com/dikolacici/DI_kolacici/domain/model/Category.java
@@ -0,0 +1,22 @@
+package com.dikolacici.DI_kolacici.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String categoryName;
+}
