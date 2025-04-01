72933e48ee9ef57cc8b0dfbf32a07c318a7f1c82
Doris Bilic <dorisbilic11@gmail.com>
2025-04-01 15:48:49 +0200

    Change DECIMAL(10,2) to DECIMAL(5,2)

--- src/main/resources/db/migration/V2.0__products_table.create.sql
+++ src/main/resources/db/migration/V2.0__products_table.create.sql
@@ -3,7 +3,7 @@ CREATE TABLE products
     id                  BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
     product_name        VARCHAR(255)              NOT NULL,
     description         TEXT,
-    price               DECIMAL(10, 2)            NOT NULL,
+    price               DECIMAL(5, 2)            NOT NULL,
     image_url           VARCHAR(255),
     category_id         BIGINT(20),
 
