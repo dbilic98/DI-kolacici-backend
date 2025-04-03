181989bdc655ddc08e92da162e05d28800acd26b
Doris Bilic <dorisbilic11@gmail.com>
2025-04-03 16:48:11 +0200

    Create Customer entity for storing user data

---
+++ src/main/java/com/dikolacici/DI_kolacici/domain/model/Customer.java
@@ -0,0 +1,30 @@
+package com.dikolacici.DI_kolacici.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+}
