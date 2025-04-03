32bc3072d5949081fd5eee343e96284c8c193c21
Doris Bilic <dorisbilic11@gmail.com>
2025-04-03 15:01:00 +0200

    Changed image_url to image_data (LONGBLOB) and added image_type

--- src/main/resources/db/migration/V2.0__products_table.create.sql
+++ src/main/resources/db/migration/V2.0__products_table.create.sql
@@ -4,7 +4,8 @@ CREATE TABLE products
     product_name        VARCHAR(255)              NOT NULL,
     description         TEXT,
     price               DECIMAL(5, 2)            NOT NULL,
-    image_url           VARCHAR(255),
+    image_data          LONGBLOB,
+    image_type          VARCHAR(255),
     category_id         BIGINT(20),
 
     CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories (id)
