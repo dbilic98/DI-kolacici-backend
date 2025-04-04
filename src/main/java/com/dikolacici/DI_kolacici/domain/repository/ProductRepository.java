70b36832a1f7a2b368f14a3c31a5204e1046c00a
Doris Bilic <dorisbilic11@gmail.com>
2025-04-04 14:58:33 +0200

    Add @Repository annotation

--- src/main/java/com/dikolacici/DI_kolacici/domain/repository/ProductRepository.java
+++ src/main/java/com/dikolacici/DI_kolacici/domain/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.dikolacici.DI_kolacici.domain.repository;
 
 import com.dikolacici.DI_kolacici.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
