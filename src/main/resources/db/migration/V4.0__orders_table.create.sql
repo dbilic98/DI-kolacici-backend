500d012607ec2b65bda3a21d4c897739e2be4daf
Doris Bilic <dorisbilic11@gmail.com>
2025-04-04 14:57:10 +0200

    Change status column type to TINYINT and add note column

--- src/main/resources/db/migration/V4.0__orders_table.create.sql
+++ src/main/resources/db/migration/V4.0__orders_table.create.sql
@@ -2,7 +2,8 @@ CREATE TABLE orders
 (
     id          BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
     order_date  DATE                      NOT NULL,
-    status      VARCHAR(20)               NOT NULL,
+    status      TINYINT,
+    note        TEXT,
     customer_id BIGINT(20),
 
     CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers (id)
