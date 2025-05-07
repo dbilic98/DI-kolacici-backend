58c58da9e51e27a27802151eb1bdbd51017fa7fd
Doris Bilic <dorisbilic11@gmail.com>
2025-05-07 10:33:48 +0200

    Update OrderService by adding ProductRepository and OrderItemRepository

--- src/main/java/com/dikolacici/DI_kolacici/domain/service/OrderService.java
+++ src/main/java/com/dikolacici/DI_kolacici/domain/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.dikolacici.DI_kolacici.domain.service;
 
 import com.dikolacici.DI_kolacici.domain.model.Order;
+import com.dikolacici.DI_kolacici.domain.repository.OrderItemRepository;
 import com.dikolacici.DI_kolacici.domain.repository.OrderRepository;
+import com.dikolacici.DI_kolacici.domain.repository.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -12,8 +14,14 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    private final ProductRepository productRepository;
+
+    private final OrderItemRepository orderItemRepository;
+
+    public OrderService(OrderRepository orderRepository, ProductRepository productRepository, OrderItemRepository orderItemRepository) {
         this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
+        this.orderItemRepository = orderItemRepository;
     }
 
     public Page<Order> getAllOrders(int pageNumber, int pageSize) {
