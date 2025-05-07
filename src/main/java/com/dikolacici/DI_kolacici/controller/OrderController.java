cd05af241d2082ac1975e1045288b50bf86e0e1d
Doris Bilic <dorisbilic11@gmail.com>
2025-05-07 10:32:41 +0200

    Refactor OrderController by adding mapping for order items and replacing customerId with customer DTO

--- src/main/java/com/dikolacici/DI_kolacici/controller/OrderController.java
+++ src/main/java/com/dikolacici/DI_kolacici/controller/OrderController.java
@@ -1,11 +1,15 @@
 package com.dikolacici.DI_kolacici.controller;
 
-import com.dikolacici.DI_kolacici.controller.response.PaginatedResponse;
-import com.dikolacici.DI_kolacici.controller.response.ResponseOrderDto;
+import com.dikolacici.DI_kolacici.controller.response.*;
+import com.dikolacici.DI_kolacici.domain.model.Customer;
 import com.dikolacici.DI_kolacici.domain.model.Order;
+import com.dikolacici.DI_kolacici.domain.model.OrderItem;
 import com.dikolacici.DI_kolacici.domain.service.OrderService;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
@@ -13,8 +17,44 @@ public class OrderController {
 
     private final OrderService orderService;
 
+
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+    private List<ResponseOrderItemDto> mapOrderItemsToResponseDtos(List<OrderItem> orderItems) {
+        List<ResponseOrderItemDto> responseOrderItemDtos = new ArrayList<>();
+
+        for (OrderItem orderItem : orderItems) {
+            ResponseOrderItemDto responseOrderItemDto = new ResponseOrderItemDto(
+                    orderItem.getQuantity(),
+                    orderItem.getProduct().getProductName());
+            responseOrderItemDtos.add(responseOrderItemDto);
+        }
+        return responseOrderItemDtos;
+    }
+
+    private ResponseOrderDto toResponseDto(Order order) {
+        List<ResponseOrderItemDto> responseOrderItemDtos = mapOrderItemsToResponseDtos(order.getOrderItems());
+
+        Customer customer = order.getCustomer();
+        ResponseCustomerDto responseCustomerDto = new ResponseCustomerDto(
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail(),
+                customer.getPhone(),
+                customer.getStreet(),
+                customer.getStreetNumber(),
+                customer.getCity(),
+                customer.getCountry()
+        );
+
+        return new ResponseOrderDto(
+                order.getId(),
+                order.getOrderDate(),
+                order.getStatus(),
+                order.getNote(),
+                responseCustomerDto,
+                responseOrderItemDtos);
     }
 
     @GetMapping
@@ -24,15 +64,5 @@ public class OrderController {
         Page<Order> allOrders = orderService.getAllOrders(pageNumber, pageSize);
         Page<ResponseOrderDto> map = allOrders.map(this::toResponseDto);
         return new PaginatedResponse<>(map);
-    }
-
-    public ResponseOrderDto toResponseDto(Order order) {
-        return new ResponseOrderDto(
-                order.getId(),
-                order.getOrderDate(),
-                order.getStatus(),
-                order.getNote(),
-                order.getCustomerId().getId()
-        );
     }
 }
